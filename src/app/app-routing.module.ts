4f0853dd10fa23970485126acb7c6a708035ef2e
Santosh Yadav <santosh.yadav198613@gmail.com>
2020-05-28 17:44:28 +0530

    department changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,20 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: 'employee', loadChildren: () => import('./employee/employee.module').then(m => m.EmployeeModule) }
+  {
+    path: 'employee',
+    loadChildren: () => import('./employee/employee.module').then(m => m.EmployeeModule)
+  },
+  {
+    path: 'department',
+    loadChildren: () => import('./department/department.module').then(m => m.DepartmentModule)
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     initialNavigation: 'enabled'
-})],
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
